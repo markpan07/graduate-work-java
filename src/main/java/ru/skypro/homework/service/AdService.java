97576d49fb7cb0dc91a835a9a8e953c1ba489f4a
ПК <themorozushka@gmail.com>
2024-03-13 20:13:27 +0300

    AdService is done.

---
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -0,0 +1,33 @@
+package ru.skypro.homework.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.ad.AdDto;
+import ru.skypro.homework.dto.ad.AdsDto;
+import ru.skypro.homework.dto.ad.CreateOrUpdateAdDto;
+import ru.skypro.homework.dto.ad.ExtendedAdDto;
+import ru.skypro.homework.entity.Ad;
+
+import java.io.IOException;
+
+public interface AdService {
+
+
+    AdDto createAd(CreateOrUpdateAdDto createOrUpdateAdDto, MultipartFile image, Authentication authentication) throws IOException;
+
+    AdsDto getAll();
+
+    AdsDto getMyAds(Authentication authentication);
+
+    Ad getAd(Integer id);
+
+    byte[] getImage(Integer id) throws IOException;
+
+    ExtendedAdDto getExtendedAd(Integer id);
+
+    AdDto updateAd(Integer id, CreateOrUpdateAdDto createOrUpdateAdDto, Authentication authentication);
+
+    void deleteAd(Integer id, Authentication authentication);
+
+
+}
